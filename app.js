8d8eadf6603045defd9049c375d7b4a022eda37f
amrelhewy <amrelhewi@gmail.com>
2020-07-01 21:02:29 +0200

    fix

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ const express=require('express');
 const app=express();
 const cors=require('cors');
 const brain=require('brain.js');
-const data=require('./data.json')
-const net = new brain.recurrent.LSTM({outputSize:4});
-let trainingData;
- async function trainer(){
-    let data=await net.train(data,{iterations:2000,log:stats=>console.log(stats)})
-    return data;
-}
-trainingData= trainer();
 
+const net = new brain.recurrent.LSTM({hiddenLayers:[5,5]});
+const dataa=require('./data.json')
+ net.train(dataa,{iterations:100,log:stats=>console.log(stats)})
 
 const PORT=process.env.PORT || 5000;
 app.use(express.json());
@@ -23,9 +18,7 @@ app.use(express.static(__dirname+'/build'));
 app.post('/find',async(req,res)=>{
 const {category,duration,place}=req.body;
 const output=net.run([`${category}`,`${duration}`,`${place}`]);
-console.log(output)
 res.status(200).send(output);
-
 });
 
 app.get('*',(req,res)=>{
