1252c90c1de552966051c3c0c14d37b19bb93b36
amrelhewy <amrelhewi@gmail.com>
2020-07-01 20:44:34 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,29 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const brain=require('brain.js');
+const data=require('./data.json')
+const net = new brain.recurrent.LSTM({outputSize:4});
+
+let trainingData=net.train(data,{iterations:2000,log:stats=>console.log(stats)})
+console.log(trainingData)
+
+const PORT=process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors({maxAge:86400,origin:'http://localhost:3000'}));
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+app.use(express.static(__dirname+'/build'));
+
+app.post('/find',async(req,res)=>{
+const {category,duration,place}=req.body;
+const output=net.run([`${category}`,`${duration}`,`${place}`]);
+console.log(output)
+res.status(200).send(output);
+
+});
+
+app.get('*',(req,res)=>{
+    res.sendFile(__dirname+'/build/index.html')
+})
---
+++ build/precache-manifest.0cd4627bbd70c8b5e9e2109cddd7651f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d43698fe84c0709a60fc96716f47632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3c0fc48f056850988ba",
+    "url": "/static/css/main.35235f3a.chunk.css"
+  },
+  {
+    "revision": "c10f64700f24db20590c",
+    "url": "/static/js/2.5149c1ee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5149c1ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c0fc48f056850988ba",
+    "url": "/static/js/main.c4d276bd.chunk.js"
+  },
+  {
+    "revision": "80719f27b35e9522ea2f",
+    "url": "/static/js/runtime-main.bdde5400.js"
+  },
+  {
+    "revision": "daf9f43ee339c496192ebf5c0142b5e8",
+    "url": "/static/media/logo_transparent.daf9f43e.png"
+  }
+]);
